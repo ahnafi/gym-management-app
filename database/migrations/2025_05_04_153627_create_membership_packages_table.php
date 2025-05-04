01da2b05894466fb2caa96a51b7b9b6df50afbe5
athallahdx <athallahgamer4@gmail.com>
2025-05-04 23:12:45 +0700

    Add: Create migration files for membership database schema

---
+++ database/migrations/2025_05_04_153627_create_membership_packages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('membership_packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('duration')->default(0); // Duration in days
+            $table->integer('price')->default(0);
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->string('images')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('membership_packages');
+    }
+};
---
+++ database/migrations/2025_05_04_154204_create_membership_histories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('membership_histories', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('start_date');
+            $table->datetime('end_date');
+            $table->enum('status', ['active', 'expired'])->default('active');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('membership_package_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('membership_histories');
+    }
+};
