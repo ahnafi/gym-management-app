3794fa446b2c757956c4884c0c88994609619dca
athallahdx <athallahgamer4@gmail.com>
2025-05-28 10:43:22 +0700

    feat: Implement membership and trainer packages methods in CatalogController

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -11,6 +11,26 @@ use function Termwind\render;
 
 class CatalogController extends Controller
 {
+    public function membershipPackages()
+    {
+        $membershipPackages = MembershipPackage::active()
+            ->get()
+            ->map(function ($package) {
+                $package->duration_in_months = round($package->duration / 30, 1);
+                return $package;
+            });
+
+
+        return Inertia::render('membershipPackages/index', compact('membershipPackages'));
+    }
+
+    public function trainerPackages()
+    {
+        $trainerPackages = PersonalTrainerPackage::all();
+
+        return Inertia::render('personalTrainerPackages/index', compact('trainerPackages'));
+    }
+
     public function gymClasses()
     {
         $gymClasses = GymClass::all();
@@ -25,20 +45,6 @@ class CatalogController extends Controller
 
     public function personalTrainers() {
         return Inertia::render('personalTrainer/index');
-    }
-
-    public function trainerPackages()
-    {
-        $trainerPackages = PersonalTrainerPackage::all();
-
-        return Inertia::render('personalTrainerPackages/index', compact('trainerPackages'));
-    }
-
-    public function membershipPackages()
-    {
-        $membershipPackages = MembershipPackage::all();
-
-        return Inertia::render('membershipPackages/index', compact('membershipPackages'));
     }
 
 }
