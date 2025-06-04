7706c56ad4b6a186b6dda469baf1f18bd58cede9
athallahdx <athallahgamer4@gmail.com>
2025-06-04 19:02:26 +0700

    Add: Provide user transactional history data fetching

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -2,6 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\GymClassAttendance;
+use App\Models\MembershipHistory;
+use App\Models\PersonalTrainerAssignment;
+use App\Models\MembershipPackage;
+use App\Models\GymClass;
+use App\Models\PersonalTrainerPackage;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,21 +17,60 @@ class HistoryController extends Controller
 {
     public function paymentHistory()
     {
-        return Inertia::render("history/paymentHistory");
+        $paymentHistories = Transaction::query()
+            ->where('user_id', auth()->id())
+            ->with([
+                'purchasable' => function (MorphTo $morphTo) {
+                    $morphTo->morphWith([
+                        MembershipPackage::class => ['id', 'code', 'name'],
+                        GymClass::class => ['id', 'code', 'name'],
+                        PersonalTrainerPackage::class => ['id', 'code', 'name'],
+                    ]);
+                }
+            ])
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return Inertia::render("history/paymentHistory/index", compact('paymentHistories'));
     }
+
 
     public function gymClassHistory()
     {
-        return Inertia::render("history/gymClassHistory");
+        $gymClassHistories = GymClassAttendance::query()
+            ->where('user_id', auth()->id())
+            ->with([
+                'gymClassSchedule:id,date,start_time,end_time',
+                'gymClassSchedule.gymClass:id,code,name,images'
+            ])
+            ->orderBy('created_at', 'desc')
+            ->get(['id', 'user_id', 'gym_class_schedule_id', 'created_at']);
+
+        return Inertia::render("history/gymClassHistory/index");
     }
 
     public function personalTrainingHistory()
     {
-        return Inertia::render("history/personalTrainingHistory");
+        $personalTrainingHistories = PersonalTrainerAssignment::query()
+            ->with([
+                'PersonalTrainerSchedules:id,scheduled_at,check_in_time,check_out_time',
+                'personalTrainerPackage:id,code,name,price,day_duration,images',
+                'personalTrainerPackage.personalTrainer:id,name,images'
+            ])
+            ->where('user_id', auth()->id())
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return Inertia::render("history/personalTrainingHistory/index", compact('personalTrainingHistories'));
     }
 
     public function membershipHistory()
     {
-        return Inertia::render("history/membershipHistory");
+         $membershipHistories = MembershipHistory::query()
+            ->where('user_id', auth()->id())
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        return Inertia::render("history/membershipHistory/index", compact('membershipHistories'));
     }
 }
