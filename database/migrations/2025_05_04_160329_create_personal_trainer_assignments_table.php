8c0145215ecd0ac187970052a477d15e634f7963
athallahdx <athallahgamer4@gmail.com>
2025-06-10 21:53:57 +0700

    Modify: make end_date nullable in personal_trainer_assignments table

--- database/migrations/2025_05_04_160329_create_personal_trainer_assignments_table.php
+++ database/migrations/2025_05_04_160329_create_personal_trainer_assignments_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->integer('day_left');
             $table->datetime('start_date');
-            $table->datetime('end_date');
+            $table->datetime('end_date')->nullable();
             $table->enum('status', ['active', 'cancelled', 'completed'])->default('active');
 
             $table->foreignId('user_id')->constrained('users');
@@ -24,7 +24,6 @@ return new class extends Migration
 
             $table->timestamps();
         });
-
     }
 
     /**
